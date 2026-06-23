406295a7a0392211e15da8a5f4b57eedf33d807b
zelttikDevv <alberrttoop@gmail.com>
2026-06-23 17:08:35 -0500

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Rifas con causa Cozumel",
+  description: "Plataforma de rifas para apoyar a nuestra comunidad",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="es">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
