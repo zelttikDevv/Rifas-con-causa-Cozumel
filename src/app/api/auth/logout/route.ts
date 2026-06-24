6f9eb685d709c9a00cebb8e139c67c81ba24e956
zelttikDevv <alberrttoop@gmail.com>
2026-06-23 23:33:28 -0500

    Update route.ts

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server';
 
 export async function POST() {
   const response = NextResponse.json({ ok: true, mensaje: 'Logout exitoso' });
+  
   response.cookies.delete('admin_session');
+  
   return response;
 }
